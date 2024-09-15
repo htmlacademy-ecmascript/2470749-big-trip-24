18ac08009e30caac0e24d02978e9bc504ae93323
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-09-15 18:12:18 +0300

    feat: add private class fields, add getters instead of functions

--- src/presenter/main-presenter.js
+++ src/presenter/main-presenter.js
@@ -7,25 +7,30 @@ import { render } from '../framework/render';
 
 
 export default class MainPresenter {
-  pointsListComponent = new PointListView();
+  #pointsListComponent = new PointListView();
+  #pointsContainer = null;
+  #pointModel = null;
+  #points = [];
+  #destinations = [];
+  #offers = [];
 
   constructor({pointsContainer, pointModel}) {
-    this.pointsContainer = pointsContainer;
-    this.pointModel = pointModel;
+    this.#pointsContainer = pointsContainer;
+    this.#pointModel = pointModel;
   }
 
   init() {
-    this.points = [...this.pointModel.getPoints()];
-    this.destinations = [...this.pointModel.getDestinations()];
-    this.offers = [...this.pointModel.getOffers()];
+    this.#points = [...this.#pointModel.points];
+    this.#destinations = [...this.#pointModel.destinations];
+    this.#offers = [...this.#pointModel.offers];
 
-    render(new SortingView(), this.pointsContainer);
-    render(this.pointsListComponent, this.pointsContainer);
-    render(new EditPoint({point: this.points[0], offers: this.offers, destinations: this.destinations}), this.pointsListComponent.element);
-    render(new CreatePoint(), this.pointsListComponent.element);
+    render(new SortingView(), this.#pointsContainer);
+    render(this.#pointsListComponent, this.#pointsContainer);
+    render(new EditPoint({point: this.#points[0], offers: this.#offers, destinations: this.#destinations}), this.#pointsListComponent.element);
+    render(new CreatePoint(), this.#pointsListComponent.element);
 
-    for (let i = 1; i < this.points.length; i++) {
-      render(new PointItemView({point: this.points[i], offers: this.offers, destinations: this.destinations}), this.pointsListComponent.element);
+    for (let i = 1; i < this.#points.length; i++) {
+      render(new PointItemView({point: this.#points[i], offers: this.#offers, destinations: this.#destinations}), this.#pointsListComponent.element);
     }
   }
 }
