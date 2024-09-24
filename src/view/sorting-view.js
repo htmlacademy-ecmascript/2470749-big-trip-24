5ee999a43e45a543a941bfa515541dcfb5a30fe1
Ksenia Tryapitsyna <k.tryapitsyna@gmail.com>
2024-09-24 16:53:14 +0300

    fix: make changes after lint check

--- src/view/sorting-view.js
+++ src/view/sorting-view.js
@@ -3,11 +3,11 @@ import AbstractView from '../framework/view/abstract-view';
 import { SortType } from '../const';
 
 
-const getSortingItems = (sorting, currentSortType) => {
+const getSortingItems = (sorting, currentSortType) =>
   // console.log(`sorting ${ sorting}`);
   // console.log(`current ${ currentSortType}`);
   // console.log(sorting === currentSortType ? 'checked' : '');
-  return `<div class="trip-sort__item  trip-sort__item--${sorting}">
+  `<div class="trip-sort__item  trip-sort__item--${sorting}">
   <input
   id="sort-${sorting}"
   class="trip-sort__input  visually-hidden"
@@ -18,8 +18,6 @@ const getSortingItems = (sorting, currentSortType) => {
   ${sorting === currentSortType ? 'checked' : ''}>
 <label class="trip-sort__btn" for="sort-${sorting}" data-sort-type="${sorting}">${capitalize(sorting)}</label>
 </div>`;
-};
-
 function createSortingTemplate(currentSortType) {
   return `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
   ${Object.values(SortType).map((sorting) => getSortingItems(sorting, currentSortType)).join('')}
